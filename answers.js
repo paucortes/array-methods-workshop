d008ae2d5bf647b65184ba159a3538e14c0451bb
Pau <paucortes2@gmail.com>
2016-04-15 19:44:19 +0000

    Completed the fourth exercise but forgot to log one of the previous ones

--- answers.js
+++ answers.js
@@ -21,7 +21,7 @@ function getPositives(num) {
 var filteredArr2 = arr2.filter(getPositives);
 // console.log(filteredArr2);
 
-// Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
+// Exercise 3: Write a function called filterArray that takes an array AND a function as arguments. Your filter function should return a new array that contains only the elements where the passed function returns a truthy value.
 // NOTE: You are allowed to use Array.prototype.filter to answer this question.
 // NOTE 2: This is a bit of a trick question, the answer is a one-liner :)
 
@@ -37,3 +37,19 @@ function filterArray(arr, func) {
     return arr.filter(func);
 }
 // console.log(filterArray(arr3, onlyTruthy));
+
+// Exercise 4: Write a function called longestWord that takes a string as argument, and returns the longest word in the string. You should use Array.prototype.reduce to do your work.
+// Hint: You can use String.prototype.split to split the string into an array of words.
+
+
+function longestWord(string) {
+    var splitWords = string.split(" ");
+    return splitWords.reduce(function(prev, next) {
+        if (prev.length < next.length) {
+            prev = next;
+        }
+        return prev;
+})}
+
+// console.log(longestWord("Let's see how this workjrkfrbgs"));
+
